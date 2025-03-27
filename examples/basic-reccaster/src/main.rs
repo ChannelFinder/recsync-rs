ae88e50677d6e284aca7aa1979bf710a23618f54
Aqeel AlShafei <aqeel.alshafei@stfc.ac.uk>
2025-03-27 09:06:44 +0000

    Update reccaster rust example to refect changes

--- examples/basic-reccaster/src/main.rs
+++ examples/basic-reccaster/src/main.rs
@@ -5,17 +5,23 @@
 // You must comply with both licenses to use, modify, or distribute this software.
 // See the LICENSE file for details.
 
+use std::collections::HashMap;
+use tracing_subscriber;
 use reccaster::{record::Record, Reccaster};
 
 #[tokio::main]
 async fn main() {
 
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
 
     let mut record = Record::new("DEV:RECASTER:RUST".to_string(), "ai".to_string());
     record.properties.insert("recordDesc".to_string(), "Rust Recaster".to_string());
     let records: Vec<Record> = vec![record];
 
-    let mut caster = Reccaster::new(records).await;
+    let mut props:  HashMap<String, String> = HashMap::new();
+    props.insert("ENGINEER".into(), "Rust Recaster".into());
+    props.insert("HOSTNAME".into(), "Example-Host-Machine".into());
+
+    let mut caster = Reccaster::new(records, Some(props)).await;
     caster.run().await;
 }
